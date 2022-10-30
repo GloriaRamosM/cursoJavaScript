41be9e9db8de7a22ae5506a02e7d6cdf06726795
¨Gloria¨ <gloriaramosmoran@gmail.com>
2022-10-30 13:54:52 -0300

    Primera entrega con variables, use ciclos , condicionales y funciones

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ let diagnosticoTLP = 0;
 let criteriosTLP = 0;
 let noHayTLP = 0;
 
-
-
-
+/* Esta funcion es la pregunta para recolectar los datos, el descarte de criterios, 
+opciones de respuesta del usuario son A,B o C y tambien cuenta las respuestas*/
 
 function preguntar(textoPregunta) {
     let respuesta = prompt(textoPregunta)
@@ -29,72 +28,39 @@ function preguntar(textoPregunta) {
     }
 }
 
-preguntar(" 1. Realizas muchos esfuerzos para evitar el abandono o sentirte desamparado?")
+/* Esta funcion es  la que interpreta, segun la cantidad de respuestas y le muestra al usuario el resultado final */
 
-preguntar(" 2.Sientes que tus relaciones son inestables y/o intensas la mayoria de las veces?")
+function darResultados() {
+    if (diagnosticoTLP >= 5) {
+        alert("Cumples con los suficientes criterios diagnosticos para el trastorno de personalidad Limite, (estos valores son solo informativos)")
+    }
+    else if (criteriosTLP >= 5) {
+        alert("Cumples con algunos criterios diagnosticos para el trastorno de personalidad Limite , sin llegar a cumplir con los suficientes criterios para el diagnostico")
+    }
+    else {
+        alert("No cumples con los criterios de TLP")
+    }
+}
 
-preguntar("3. Te cuesta aceptarte a ti misma/o y valorar tu autoimagen?")
+/* Aca estoy usando las funciones en cada momento que las necesito,
+ tiene un orden, primero pregunta y recolecta los datos, y luego interpreta*/
 
-preguntar(" 4.En ocasiones puedes llegar a ser impulsiva/o y esas acciones pueden llegar a danarte? ej: gastar en exceso,consumir drogas")
+preguntar("1.Realizas muchos esfuerzos para evitar el abandono o sentirte desamparado?")
 
-preguntar(" 5.Algunas vez intentaste o pensaste en suicidarte?")
+preguntar("2.Sientes que tus relaciones son inestables y/o intensas la mayoria de las veces?")
 
-preguntar(" 6.Sueles sentirte inestable emocionalmente, y cambiar de un estado a otro en cortos lapsos de tiempo?")
+preguntar("3.Te cuesta aceptarte a ti misma/o y valorar tu autoimagen?")
 
-preguntar(" 7.Te sientes vacia/o la mayor parte del tiempo?")
+preguntar("4.En ocasiones puedes llegar a ser impulsiva/o y esas acciones pueden llegar a danarte? ej: gastar en exceso,consumir drogas")
+
+preguntar("5.Algunas vez intentaste o pensaste en suicidarte?")
+
+preguntar("6.Sueles sentirte inestable emocionalmente, y cambiar de un estado a otro en cortos lapsos de tiempo?")
+
+preguntar("7.Te sientes vacia/o la mayor parte del tiempo?")
 
 preguntar("8.Sueles enojarte facilmente, llegando a estados de ira y/o peleas que te cuestan controlar?")
 
-preguntar("9. Tienes ideas paranoides ocasionalmente, sobre todo si te sientes estresada/o?")
+preguntar("9.Tienes ideas paranoides ocasionalmente, sobre todo si te sientes estresada/o?")
 
-console.log( diagnosticoTLP, criteriosTLP,noHayTLP)
-
-
-
-function darResultados (){
-    
-
-}
-
-
-
-
-/*
-
-
-let pregunta2 = prompt(" Tus relaciones son inestables y/o por lo general intensas?")
-while (pregunta2 != "ESC") {
-    switch .(pregunta2) {
-        case "A":
-            break;
-        case "B":
-            break;
-        case "C":
-            break;
-        default:
-
-    }
-
-    break
-
-}
-
-let pregunta3 = prompt(" Dificultad para aceptarte a ti misma/o y tu imagen corporal")
-while (pregunta3 != "ESC") {
-    switch (pregunta3) {
-        case "A":
-            break;
-        case "B":
-            break;
-        case "C":
-            break;
-        default:
-
-    }
-
-    break
-
-}
-*/
-
-
+darResultados()
